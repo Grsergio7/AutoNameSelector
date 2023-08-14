3b76f9aa06ff96c6706db2732fcef23847e18d33
Sergio <ruiz.sergio07@gmail.com>
2023-08-14 13:56:50 -0700

    Modified UI by using Grid instead of Place

--- tests/ui_test.py
+++ tests/ui_test.py
@@ -1,52 +1,38 @@
 import tkinter
 from tkinter import *
 
-names = ["Emerald", "Mila", "Ricky"]
+# Create an instance of tkinter frame
+win = Tk()
 
-def set_text(label, text):
-    label.delete(0, END)
-    label.insert(0, text)
-    return
+# Define geometry of the window
+win.geometry("700x250")
 
-# setup size of window
-window = Tk()
-window.title("Auto Name Selector")
-window.geometry("600x600")
+# Functions
+def set_text(field, text):
+   field.delete(0,END)
+   field.insert(0, text)
 
 # setup Labels
-# video = Label(window, 
-#                  text="Video").place(x = 10,
-#                                              y = 25)
-# audio = Label(window, 
-#                  text='Audio').place(x = 10,
-#                                              y = 50)
-# av_Accomodador = Label(window, 
-#                           text='AV Acomodador').place(x = 10,
-#                                                       y = 75)
-# mic1 = tk.Label(window, text='Mic 1').
-# mic2 = tk.Label(window, text='Mic 2').
-# plat = tk.Label(window, text='Platform')
-# door = tk.Label(window, text='Puerta')
-# acco1 = tk.Label(window, text='Acomodador A')
-# acco2 = tk.Label(window, text='Acomodador B')
+video_label = Label(win, text="Video")
+audio_label = Label(win, text="Audio")
+av_label = Label(win, text="AV Accomodador")
 
 # Setup Fields
-# video_field = Entry(window,
-#                        width=20).place(x = 200,
-#                                        y = 25)
-# audio_field = Entry(window,
-#                        width=20).place(x = 200,
-#                                        y = 50)
-# avAcco = Entry(window,
-#                        width=20).place(x = 200,
-#                                        y = 75)
-v = Entry(window, width=30).place(x=10,y=20)
+video_field = Entry(win, bg="white", width=20, borderwidth=2)
+audio_field = Entry(win, bg="white", width=20, borderwidth=2)
+av_field = Entry(win, bg="white", width=20, borderwidth=2)
 
-# Create Button for random (delete and insert only work when you pack())
-video_Button = Button(window,
-                         text="Randomize", width=6, command=lambda:set_text("Sergio Ruiz")).place(x = 400,
-                                                 y = 75)
+# Place in window using grid
+video_label.grid(row=0)
+audio_label.grid(row=0,column=2)
+av_label.grid(row=0,column=3)
 
+video_field.grid(row=1)
+audio_field.grid(row=1,column=2)
+av_field.grid(row=1,column=3)
+
+# Create Button for random 
+av_button = Button(win, text="Choose", command=lambda:set_text(video_field ,"name")).grid(row=1,column=4)
 
 # Calls the window object
-window.mainloop()
+win.mainloop()
